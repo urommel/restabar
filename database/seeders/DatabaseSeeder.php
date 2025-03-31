90ff209609ecfa10418fdb83fb2d1d5e6b019cb0
Rommel Ulco <rulco@unitru.edu.pe>
2025-03-31 00:27:00 -0500

    Agregar datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Role;
+use App\Models\MenuEntry;
+use App\Models\Table;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +20,89 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Susan',
+            'email' => 'susan@example.com',
+            'role' => Role::FRONTLINE,
         ]);
+        User::factory()->create([
+            'name' => 'John',
+            'email' => 'johnn@example.com',
+            'role' => Role::KITCHEN,
+        ]);
+
+        Table::factory()->count(5)
+            ->state(new Sequence(
+                ['id' => 1, 'name' => 'Table 1'],
+                ['id' => 2, 'name' => 'Table 2'],
+                ['id' => 3, 'name' => 'Table 3'],
+                ['id' => 4, 'name' => 'Table 4'],
+                ['id' => 5, 'name' => 'Table 5'],
+            ))
+            ->create();
+        MenuEntry::factory()->count(10)
+            ->state(new Sequence(
+                [
+                    'id' => 1,
+                    'name' => 'Burger Clásica',
+                    'description' => '200 gr de carne, queso cheddar, vegetales',
+                    'price' => 550
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Doble Queso',
+                    'description' => 'Carne, doble queso cheddar, vegetales',
+                    'price' => 750
+                ],
+                [
+                    'id' => 3,
+                    'name' => 'Veggie Deluxe',
+                    'description' => 'Hamburguesa de lentejas, vegetales, aguacate',
+                    'price' => 600
+                ],
+                [
+                    'id' => 4,
+                    'name' => 'Swiss Burger',
+                    'description' => 'Carne, queso suizo, lechuga, tomate',
+                    'price' => 650
+                ],
+                [
+                    'id' => 5,
+                    'name' => 'Bacon Cheese',
+                    'description' => 'Queso cheddar, bacon, vegetales',
+                    'price' => 700
+                ],
+                [
+                    'id' => 6,
+                    'name' => 'BBQ Especial',
+                    'description' => 'Carne, salsa BBQ, queso cheddar, cebolla caramelizada',
+                    'price' => 720
+                ],
+                [
+                    'id' => 7,
+                    'name' => 'Spicy Jack',
+                    'description' => 'Queso pepper jack, jalapeños, salsa picante',
+                    'price' => 680
+                ],
+                [
+                    'id' => 8,
+                    'name' => 'Mushroom Swiss',
+                    'description' => 'Carne, champiñones salteados, queso suizo, cebolla caramelizada',
+                    'price' => 800
+                ],
+                [
+                    'id' => 9,
+                    'name' => 'Truffle Brie',
+                    'description' => 'Queso brie, rúcula, tomate seco, mayonesa trufada',
+                    'price' => 720
+                ],
+                [
+                    'id' => 10,
+                    'name' => 'Buffalo Chicken',
+                    'description' => 'Pollo empanizado, salsa buffalo, lechuga, aderezo ranch',
+                    'price' => 750
+                ],
+
+            ))
+            ->create();
     }
 }
